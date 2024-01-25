524f418ad0cc341a7463d9dace443c9d58bfca5e
PrimeEagle <misc@hiddendragon.com>
2024-01-24 23:18:34 -0600

    update build conditions

--- VNet.Testing/obj/Debug/net8.0/VNet.Testing.AssemblyInfo.cs
+++ VNet.Testing/obj/Debug/net8.0/VNet.Testing.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Class library for helper functions for testing.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17b283eeaf31b5dfd49789a79c7ab4a72c51f016")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e07c883f8b4444145fdb67e1c28363c55047780")]
 [assembly: System.Reflection.AssemblyProductAttribute("VNet.Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VNet.Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
